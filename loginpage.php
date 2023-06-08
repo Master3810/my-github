bcabea942cd8559f9e450540e62886f12fb089c8
MasterMathyola <132939628+Master3810@users.noreply.github.com>
2023-06-08 22:31:31 +0200

    Add files via upload

---
+++ loginpage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>My data</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Welcome</h1>";
+        $servername ="localhost";
+        $username="root";
+        $password="";
+        $dbname="project";
+        $conn=new mysqli($servername,$username,$password,$dbname);
+        if($conn){
+            echo "succefully connected ";
+            $result=mysqli_query($conn, "select* from master");
+            while($row=mysqli_fetch_array($result)){
+                echo $row["contact"]."<br> ".$row["skill"];
+            }
+        }
+        else 
+        echo "Database connection failed";
+        ?>
+    </body>
+</html>
