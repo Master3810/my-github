586748e3914bea4615d13bf8efbc4056b3390136
master <mastermathyola3810@gmail.com>
2023-06-15 12:35:43 +0200

    update

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
+
     <head>
         <title>MY WEB</title>
+              <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css">
         <style type="text/css">
+            
             a {text-decoration: none;}
             a:active{color:red;} 
             h1{ font-family: Arial,; color:maroon; text-align:center;background-color:burlywood }
-        ul{ background-color: burlywood}
+       .nav ul{ background-color: burlywood}
         Ul {
             list-style-type: none;
             margin:0;
@@ -15,7 +18,7 @@
 
 
         }
-         li{
+         .nav li{
             float: left;
             padding: 5px 10px;
             background-color: beige;
@@ -33,17 +36,40 @@
         animation-play-state: running;
 
         }
+        footer{
+            width:100%;
+            background:white;
+            position:absolute;
+            bottom:0;
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+
+        }
+
+        footer ul{
+            display:flex;
+            overflow:hidden;
+        }
+
+        footer li{
+            padding:30px;
+            margin-right:40px;
+            overflow:hidden;
+        }
     </style>    
 </head>
 <body style="background: goldenrod;">
     <h1> MASTER MATHYOLA</h1> 
-    <div> 
+    <div class="nav"> 
 <ul>
        <li> <a href="projects.php">PROJECTS</a></li>
 <li><a href="contact.php">CONTACTS</a></li>
 <li><a href="Skills.php">SKILLS</a></li>
 </ul>
 </div> 
+
 <br> 
 <p>I am master Mathyola, a Malawian male born in 1999. Studied at Mzuzu University and major in ICT courses.</p>
 <img src="MM.jpg" >
@@ -53,4 +79,12 @@
 
 
 </body>
+<footer>
+    <ul>
+        <li><a href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i> facebook</a></li>
+        <li><a href="https://www.twitter.com"><i class="fa-brands fa-twitter"></i> twitter</a></li>
+        <li><a href="https://www.reddit.com"><i class="fa-brands fa-reddit"></i> reddit</a></li>
+    </ul>
+    <section>&copy; master 2023</section>
+</footer>
 </html>
